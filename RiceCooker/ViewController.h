142057774f005f66d3e54e994da47ea3ad695e97
zyl <751343235@qq.com>
2015-06-02 19:47:08 +0800

    Initial Commit

---
+++ RiceCooker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RiceCooker
+//
+//  Created by yi on 15-6-2.
+//  Copyright (c) 2015年 yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
